90aeeb9f48ef26b87ae88d4233304f09bd8aed16
Neckroo <neib.ouakel@gmail.com>
2022-06-09 13:57:44 +0200

    premiers updates
    ajouter un truc de profil

---
+++ vueFrontEnd/src/components/TheHeaderProfile.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref, computed, watchEffect } from 'vue';
+import TheProfile from './TheProfile.vue';
+import TheSelection from './TheSelection.vue';
+import { user } from "../state.js";
+
+
+
+</script>
+
+<template>
+    <div class="container_header_profile">
+        
+               
+      
+    </div>
+</template>
+
+
+
+<style scoped>
+.container_header_profile {
+    width: 100%;
+    min-height: 70px;
+    position: fixed;
+    top: 0;
+    display: flex;
+    flex-direction: row;
+    flex-basis: auto;
+    justify-content: space-between;
+    background-color: #007fd4;
+    border-bottom: 1px solid #E7F0FF;
+    opacity: 1;
+
+}
+
+
+</style>
--- vueFrontEnd/src/components/TheProfile.vue
+++ vueFrontEnd/src/components/TheProfile.vue
@@ -1,40 +1,92 @@
 <script setup>
-import { ref, computed, watchEffect } from 'vue';
+import { ref, computed, watchEffect } from "vue";
+import { user } from "../state.js";
+import TheHeaderProfile from './TheHeaderProfile.vue';
 
-const firstLetterName = ref('F');
+const firstLetterName = ref("F");
+const profileIsClicked = ref(false);
+
+function clickProfile() {
+  profileIsClicked.value = !profileIsClicked.value;
+}
 </script>
 
 <template>
-    <div class="profil">
-        <div class="insideProfil">
-            <span class="nom">{{firstLetterName}}</span>
 
-        </div>
+  <div class="profil" @click="clickProfile">
+    <div class="insideProfil">
+        
+      <img v-if="user=='anonymous'" src="../assets/Property 1=OFFPPic.png" alt="ProfilePic" />
+      <img v-if="user=='student'||user=='teacher'" src="../assets/Property 1=ONPPic.png" alt="ProfilePic" />
     </div>
+
+    <div class="popUpProfile" :class="profileIsClicked ? 'active' : ''">
+        <the-header-profile></the-header-profile>
+      <span class="bar" @click="clickProfile= !clickProfile"></span>
+    </div>
+  </div>
+
 </template>
 
 
 
 <style scoped>
 .profil {
-    display: flex;
-    align-items: center;
-    padding:10px 15px;
-
+  display: flex;
+  align-items: center;
+  padding: 10px 15px;
 }
 
 .insideProfil {
-    width: 40px;
-    height: 40px;
-    border: 1px solid #E7F0FF;
-    border-radius: 50px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 }
 
 .nom {
-    color: #E7F0FF;
+  color: #e7f0ff;
+}
+
+.bar {
+  width: 50px;
+  text-align: center;
+  height: 100%;
+  height: 5px;
+  margin: 0px auto 10px auto;
+  background-color: white;
+  border-radius: 5px;
+}
+
+div.popUpProfile {
+    position: fixed;
+    top: -100vh;
+    left:0;
+    display: flex;
+    flex-direction: column;
+    z-index: 10;
+    text-align: center;
+    width: 100%;
+    margin: auto;
+    height: 100%;
+    justify-content: flex-start;
+    
+    padding:10px;
+    opacity: 1;
+    border-radius: 10px;
+    transition: 0.3s;
+}
+
+div.popUpProfile.active {
+
+    z-index: 1000;
+    bottom: 0px;
+    background-color: #606060;
+    border-top: 1px solid #E7F0FF;
+    opacity: 1;
+    height: 100%;
+    transition: 0.3s;
+    top: 0;
+
 }
 </style>
