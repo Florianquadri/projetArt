ab7c83f46dd2549e4e00d20eb002be8468cfba9b
Neckroo <56031105+Neckroo@users.noreply.github.com>
2022-06-09 14:01:26 +0200

    Revert "premiers updates"

--- vueFrontEnd/src/components/TheProfile.vue
+++ vueFrontEnd/src/components/TheProfile.vue
@@ -1,92 +1,40 @@
 <script setup>
-import { ref, computed, watchEffect } from "vue";
-import { user } from "../state.js";
-import TheHeaderProfile from './TheHeaderProfile.vue';
+import { ref, computed, watchEffect } from 'vue';
 
-const firstLetterName = ref("F");
-const profileIsClicked = ref(false);
-
-function clickProfile() {
-  profileIsClicked.value = !profileIsClicked.value;
-}
+const firstLetterName = ref('F');
 </script>
 
 <template>
+    <div class="profil">
+        <div class="insideProfil">
+            <span class="nom">{{firstLetterName}}</span>
 
-  <div class="profil" @click="clickProfile">
-    <div class="insideProfil">
-        
-      <img v-if="user=='anonymous'" src="../assets/Property 1=OFFPPic.png" alt="ProfilePic" />
-      <img v-if="user=='student'||user=='teacher'" src="../assets/Property 1=ONPPic.png" alt="ProfilePic" />
+        </div>
     </div>
-
-    <div class="popUpProfile" :class="profileIsClicked ? 'active' : ''">
-        <the-header-profile></the-header-profile>
-      <span class="bar" @click="clickProfile= !clickProfile"></span>
-    </div>
-  </div>
-
 </template>
 
 
 
 <style scoped>
 .profil {
-  display: flex;
-  align-items: center;
-  padding: 10px 15px;
+    display: flex;
+    align-items: center;
+    padding:10px 15px;
+
 }
 
 .insideProfil {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
+    width: 40px;
+    height: 40px;
+    border: 1px solid #E7F0FF;
+    border-radius: 50px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
 }
 
 .nom {
-  color: #e7f0ff;
-}
-
-.bar {
-  width: 50px;
-  text-align: center;
-  height: 100%;
-  height: 5px;
-  margin: 0px auto 10px auto;
-  background-color: white;
-  border-radius: 5px;
-}
-
-div.popUpProfile {
-    position: fixed;
-    top: -100vh;
-    left:0;
-    display: flex;
-    flex-direction: column;
-    z-index: 10;
-    text-align: center;
-    width: 100%;
-    margin: auto;
-    height: 100%;
-    justify-content: flex-start;
-    
-    padding:10px;
-    opacity: 1;
-    border-radius: 10px;
-    transition: 0.3s;
-}
-
-div.popUpProfile.active {
-
-    z-index: 1000;
-    bottom: 0px;
-    background-color: #606060;
-    border-top: 1px solid #E7F0FF;
-    opacity: 1;
-    height: 100%;
-    transition: 0.3s;
-    top: 0;
-
+    color: #E7F0FF;
 }
 </style>
