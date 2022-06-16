4264e40e88998c05dbe0173790506361baa183f9
Florianquadri <florianquadri@gmail.com>
2022-06-16 10:36:28 +0200

    petit changement

--- vueFrontEnd/src/state.js
+++ vueFrontEnd/src/state.js
@@ -19,7 +19,7 @@ export const myClass = ref("IM49-1");
 
 export const typeOfAddTask = ref(null);
 
-export const classOuPrivateToFetch = ref("privsdf");
+export const classOuPrivateToFetch = ref("private");
 
 //student, teacher or secretary ou anonymous
 
